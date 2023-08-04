817a785e853011ec918abe1d3b3c5eba7edcba69
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2023-08-04 15:33:33 -0300

    LPS-189689 Make a new message that informs user about no longer recognized link

--- portal-impl/src/com/liferay/portal/action/UpdatePasswordAction.java
+++ portal-impl/src/com/liferay/portal/action/UpdatePasswordAction.java
@@ -161,6 +161,8 @@ public class UpdatePasswordAction implements Action {
 			String[] ticketParts = ticketParam.split("_");
 
 			if (ticketParts.length != 2) {
+				httpServletRequest.setAttribute("oldFormat", Boolean.TRUE);
+
 				return null;
 			}
 
