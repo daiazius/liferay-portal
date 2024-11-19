4092a2ec9179aac4fb7b7acd24fab1c5fee02b21
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2024-11-19 17:34:24 -0300

    LPD-40224 Make test more robust by checking export URL for all the search parameters

--- modules/test/playwright/tests/portal-security-audit-web/auditExport.spec.ts
+++ modules/test/playwright/tests/portal-security-audit-web/auditExport.spec.ts
@@ -10,7 +10,36 @@ import {loginTest} from '../../fixtures/loginTest';
 
 export const test = mergeTests(loginTest(), applicationsMenuPageTest);
 
-test('LPD-40224: Check if the export audit events resource URL has date parameters', async ({
+const PRIVATE_RENDER_PARAMETER_NAMESPACE = 'priv_r_p_';
+
+const dateFields = [
+	'endDateAmPm',
+	'endDateDay',
+	'endDateHour',
+	'endDateMinute',
+	'endDateMonth',
+	'endDateYear',
+	'startDateAmPm',
+	'startDateDay',
+	'startDateHour',
+	'startDateMinute',
+	'startDateMonth',
+	'startDateYear',
+];
+
+const fields = [
+	'className',
+	'classPK',
+	'clientHost',
+	'clientIP',
+	'eventType',
+	'serverName',
+	'userName',
+];
+
+const fieldsWithoutNamespace = ['groupId', 'serverPort', 'userId'];
+
+test('LPD-40224: Check if the export audit events resource URL has all search parameters', async ({
 	applicationsMenuPage,
 	page,
 }) => {
@@ -20,19 +49,26 @@ test('LPD-40224: Check if the export audit events resource URL has date paramete
 
 	await page.locator('#toggle_id_audit_event_searchtoggleAdvanced').click();
 
-	const startDateYear = await page.locator('#startDateYear');
-
-	const startDateYearValue = await startDateYear.inputValue();
-
 	await page.locator('.lexicon-icon-search').click();
 
 	await page.waitForTimeout(500);
 
-	const options = await page.getByLabel('Options');
+	// Populate map with all the date parameters
+
+	const dateValues = {};
+
+	for (const field of dateFields) {
+		const inputElement = page.locator(`#${field}`);
+		const inputValue = await inputElement.inputValue();
+
+		dateValues[field] = inputValue;
+	}
+
+	const options = page.getByLabel('Options');
 
 	await options.click();
 
-	const menuItem = await page.getByRole('menuitem', {
+	const menuItem = page.getByRole('menuitem', {
 		name: 'Export Audit Events',
 	});
 
@@ -40,7 +76,19 @@ test('LPD-40224: Check if the export audit events resource URL has date paramete
 
 	const downloadPromise = await page.waitForEvent('download');
 
-	await expect(downloadPromise.url()).toContain(
-		`startDateYear=${startDateYearValue}`
-	);
+	const exportURL = downloadPromise.url();
+
+	// Check if the URL has all the search parameters
+
+	for (const field of dateFields) {
+		expect(exportURL).toContain(`${field}=${dateValues[field]}`);
+	}
+
+	for (const field of fields) {
+		expect(exportURL).toContain(PRIVATE_RENDER_PARAMETER_NAMESPACE + field);
+	}
+
+	for (const field of fieldsWithoutNamespace) {
+		expect(exportURL).toContain(field);
+	}
 });
