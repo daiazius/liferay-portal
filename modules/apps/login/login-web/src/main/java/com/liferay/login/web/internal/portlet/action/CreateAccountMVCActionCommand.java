964436943d5431db8727b83512082a2414bbe2a4
Istvan Sajtos <istvan.sajtos@liferay.com>
2024-12-17 22:31:46 +0100

    LPD-9391 Update usages

--- modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/action/CreateAccountMVCActionCommand.java
+++ modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/action/CreateAccountMVCActionCommand.java
@@ -207,7 +207,7 @@ public class CreateAccountMVCActionCommand extends BaseMVCActionCommand {
 		try {
 			if (cmd.equals(Constants.ADD)) {
 				CaptchaConfiguration captchaConfiguration =
-					getCaptchaConfiguration();
+					getCaptchaConfiguration(actionRequest);
 
 				if (captchaConfiguration.createAccountCaptchaEnabled()) {
 					CaptchaUtil.check(actionRequest);
@@ -364,12 +364,14 @@ public class CreateAccountMVCActionCommand extends BaseMVCActionCommand {
 		}
 	}
 
-	protected CaptchaConfiguration getCaptchaConfiguration()
+	protected CaptchaConfiguration getCaptchaConfiguration(
+			ActionRequest actionRequest)
 		throws CaptchaConfigurationException {
 
 		try {
-			return _configurationProvider.getSystemConfiguration(
-				CaptchaConfiguration.class);
+			return _configurationProvider.getCompanyConfiguration(
+				CaptchaConfiguration.class,
+				_portal.getCompanyId(actionRequest));
 		}
 		catch (Exception exception) {
 			throw new CaptchaConfigurationException(exception);
